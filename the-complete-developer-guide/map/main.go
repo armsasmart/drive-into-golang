cdd77eb03d86b27d2632b3e249c4c3836357cab1
Sirawit <sirawit.aarm@gmail.com>
2022-08-14 17:47:09 +0700

    feat: :sparkles: create map

---
+++ the-complete-developer-guide/map/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+		"white": "#ffffff",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
